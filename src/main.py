c0169506f2cdb2a294f2dd5c398c43f52d31d3d0
sg1501 <sujanvarma.guntimaduguvenkata@unh.edu>
2022-12-08 18:23:07 -0500

    check error

--- src/main.py
+++ src/main.py
@@ -1,6 +1,6 @@
 
 """
-main.py file to answer the queries for the Unicorn dataset
+main.py file to answer the queries for the ipl dataset
 """
 
 
@@ -68,8 +68,8 @@ class Players:
         for row in self.ref:
             if row[2] != 'NULL':
                 dob = int(row[2].split("-")[-1])
-                if (row[4] == "Right-arm offbreak") and (row[3] == "Right_Hand"
-                ) and (80 < dob < 90):
+                if (row[4] == "Right-arm offbreak") and\
+                    (row[3] == "Right_Hand") and (80 < dob < 90):
                     result.add((row[1], row[-3]))
         return result
 
